6e37bfd749732a6b8b59b7d7d327bfc76efa6164
amirghedira <amirghedira06@gmail.com>
2021-10-01 03:38:04 +0100

    removing all projects

--- pages/index.js
+++ pages/index.js
@@ -80,7 +80,7 @@ export const getServerSideProps = async () => {
     const resultCount = await Axios.get('https://mywebrestapi.herokuapp.com/topic/counttopic')
     return {
         props: {
-            projects: result.data.result,
+            projects: [],
             topicsCount: resultCount.data.result
         },
     }
