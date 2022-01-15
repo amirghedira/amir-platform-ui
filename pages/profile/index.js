39ec2b83f0e6e6dd5affec7f152036744c2d147a
amirghedira <amirghedira06@gmail.com>
2022-01-15 01:42:08 +0100

    fixing seo

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -80,8 +80,8 @@ const ProfilePage = ({ UserProfile }) => {
                 <meta property="og:title" content={UserProfile.name} />
                 <meta property="og:description" content={UserProfile.aboutme} />
                 <meta property="og:image" content={UserProfile.profileimage} />
-                <meta name="robots" content="index,follow" />
-                <meta name="googlebot" content="index,follow" />
+                <meta name="robots" content="index" />
+                <meta name="googlebot" content="index" />
 
             </Head>
             <main>
--- pages/project/[projectname]/[technologies]/[projectId].js
+++ pages/project/[projectname]/[technologies]/[projectId].js
@@ -261,8 +261,8 @@ const Details = (props) => {
                 <link rel="canonical" href={`https://www.amir-ghedira.com/project/${project.name.replace(/ /g, '-')}/${project.technologie.replace(/ /g, '-')}/${project._id}`} />
                 <meta property="og:url" content={`https://www.amir-ghedira.com/project/${project.name.replace(/ /g, '-')}/${project.technologie.replace(/ /g, '-')}/${project._id}`} />
                 <meta property="og:title" content={project.name} />
-                <meta name="robots" content="index,nofollow" />
-                <meta name="googlebot" content="index,nofollow" />
+                <meta name="robots" content="index" />
+                <meta name="googlebot" content="index" />
                 <meta property="og:description" content={project.overview} />
                 {project.imagesurl.length > 0 ?
                     <meta property="og:image" content={project.imagesurl[Math.floor(Math.random() * project.imagesurl.length)]} />
