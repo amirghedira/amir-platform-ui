6f45254d85de6dc2dac3eba01eb47c9d4770da5e
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-10-31 05:16:35 +0100

    add notify stage

---
+++ ci/notify.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+VERSION="${CI_COMMIT_BRANCH}-${CI_COMMIT_SHORT_SHA}"
+SLACK_USERNAME="amir_platform_ui"
+SLACK_EMOJI=":amir_platform_ui:"
+SLACK_CHANNEL="#amir-platform-ui"
+slack_notif() {
+    token="${1}"
+    if [[ $token ]]; then
+        message="New Deployment of amir-platform ui Version: ${VERSION}"
+        endpoint="https://hooks.slack.com/services/${token}"
+        payload="{\"text\": \"${message}\", \"username\": \"${SLACK_USERNAME}\", \"channel\": \"${SLACK_CHANNEL}\", \"icon_emoji\": \"${SLACK_EMOJI}\"}"
+        curl -X POST "${endpoint}" -H "Accept: application/json" -d "${payload}"
+    fi
+}
+
+slack_notif "${SLACK_TOKEN}"
