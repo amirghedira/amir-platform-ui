9b155540f21c87199704c6d763d3c03cfb9b4dd4
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-10-31 06:01:50 +0100

    adding env variables

--- ci/deliver.sh
+++ ci/deliver.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 
-ENV_FILE=".env"
-
-echo "API_URL=${API_URL}" >> "${ENV_FILE}"
-
-
 export VERSION="${CI_COMMIT_BRANCH}-${CI_COMMIT_SHORT_SHA}"
 docker login "${CI_REGISTRY}" --username "${CI_REGISTRY_USER}" --password "${CI_REGISTRY_PASSWORD}"
 docker-compose -f docker-compose-build.yml build  "${IMAGE_NAME}"
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+
+ENV_FILE=".env"
+
+echo "API_URL=${API_URL}" >> "${ENV_FILE}"
+
 export VERSION="${CI_COMMIT_BRANCH}-${CI_COMMIT_SHORT_SHA}"
 docker login "${CI_REGISTRY}" --username "${CI_REGISTRY_USER}" --password "${CI_REGISTRY_PASSWORD}"
 docker-compose up -d --force-recreate
