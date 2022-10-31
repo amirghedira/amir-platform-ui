831601ca0e372e381b8d4246957386168591d875
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-10-31 05:17:59 +0100

    add notify stage

--- ci/deliver.sh
+++ ci/deliver.sh
@@ -6,7 +6,7 @@ ENV_FILE=".env"
 echo "API_URL=${API_URL}" >> "${ENV_FILE}"
 
 
-VERSION="${CI_COMMIT_BRANCH}-${CI_COMMIT_SHORT_SHA}"
+export VERSION="${CI_COMMIT_BRANCH}-${CI_COMMIT_SHORT_SHA}"
 docker login "${CI_REGISTRY}" --username "${CI_REGISTRY_USER}" --password "${CI_REGISTRY_PASSWORD}"
 docker-compose -f docker-compose-build.yml build  "${IMAGE_NAME}"
 docker tag "${IMAGE_NAME}:latest" "${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:${VERSION}"
