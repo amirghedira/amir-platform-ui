b69a0a47563f1776671753c9788288fd677b8b0f
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-04 06:54:52 +0100

    adding mirror stage

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-git pull origin main || :
-git remote add github https://github.com/amirghedira/mypersonalweb-nextjs.git
+REPO_PATH="/home/centos/save-amir-platform-ui/"
+if [ ! -d "${REPO_PATH}" ]; then
+    cd "${REPO_PATH}"
+    git clone https://gitlab.com/amir-platform/amir-platform-ui.git save-amir-platform-ui
+    git remote add github https://github.com/amirghedira/mypersonalweb-nextjs.git   
+fi
+
+cd "${REPO_PATH}" && git pull origin main || :
 git push github main -f
 exit 0
