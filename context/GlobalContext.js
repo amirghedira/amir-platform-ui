33a69a639451d4c35649bcead617bd7b43f7f043
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-08 10:07:14 +0100

    improving topics/context data

--- context/GlobalContext.js
+++ context/GlobalContext.js
@@ -1,11 +1,2 @@
 import React from 'react';
-export default React.createContext({
-    projects: null,
-    token: null,
-    UserProfile: null,
-    questions: null,
-    suggestions: null,
-    bannedUsers: null,
-    news: null
-
-})
+export default React.createContext({})
