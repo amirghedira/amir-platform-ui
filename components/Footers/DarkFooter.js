ea615e4bc33cdf86e03ea900fa5737ed13b236f6
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-13 18:45:18 +0100

    update linkedin link

--- components/Footers/DarkFooter.js
+++ components/Footers/DarkFooter.js
@@ -3,31 +3,31 @@ import { Container } from "reactstrap";
 import Link from "next/link";
 
 function DarkFooter() {
-  return (
-    <footer className="footer" data-background-color="black">
-      <Container>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/aboutus"> About us </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="copyright" id="copyright">
-          © {new Date().getFullYear()}{" "}
-          Coded by{" "}
-          <a
-            href="https://www.facebook.com/ghedira.amir"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Amir Ghedira
-          </a>
-          .
-        </div>
-      </Container>
-    </footer>
-  );
+    return (
+        <footer className="footer" data-background-color="black">
+            <Container>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href="/aboutus"> About us </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="copyright" id="copyright">
+                    © {new Date().getFullYear()}{" "}
+                    Coded by{" "}
+                    <a
+                        href="https://www.linkedin.com/in/amirghedira"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Amir Ghedira
+                    </a>
+                    .
+                </div>
+            </Container>
+        </footer>
+    );
 }
 
 export default DarkFooter;
