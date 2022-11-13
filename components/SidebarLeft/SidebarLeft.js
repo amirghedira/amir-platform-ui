aff73935ec75ef2ab5eec3d9b78360b9edb1f7b0
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-13 18:50:34 +0100

    improving main page layout

--- components/SidebarLeft/SidebarLeft.js
+++ components/SidebarLeft/SidebarLeft.js
@@ -4,98 +4,96 @@ import { Row, Col } from 'reactstrap';
 import Link from 'next/link'
 const Sidebar = (props) => {
     return (
-        <div className={classes.sidebar}>
-            <Row>
-                <Col className={classes.content} >
-                    <Row style={{ marginBottom: '20px' }}>
-                        <Col>
-                            <Row style={{ marginBottom: '20px' }} >
-                                <Col style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
-                                    <i className="far fa-arrow-alt-circle-right" style={{ fontSize: '17px' }}></i>
-                                    <p className={classes.titles}>Questions/Answer</p>
-                                </Col>
-                            </Row>
-                            <Row style={{ marginLeft: '10px' }}>
-                                <Col>
-                                    <Row style={{ marginBottom: '10px' }}>
-                                        <Col>
-                                            <Link
-                                                href="/topics/questions"
-                                                className={classes.items}
-                                                className={classes.navItemContainer}>
 
-                                                <i className={`far fa-question-circle ${classes.icon}`}></i>
-                                                <h5 className={classes.navItemText}>Questions</h5>
+        <Row className={classes.sidebar}>
+            <Col className={classes.content} >
+                <Row style={{ marginBottom: '20px' }}>
+                    <Col>
+                        <Row style={{ marginBottom: '20px' }} >
+                            <Col style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+                                <i className="far fa-arrow-alt-circle-right" style={{ fontSize: '17px' }}></i>
+                                <p className={classes.titles}>Questions/Answer</p>
+                            </Col>
+                        </Row>
+                        <Row style={{ marginLeft: '10px' }}>
+                            <Col>
+                                <Row style={{ marginBottom: '10px' }}>
+                                    <Col>
+                                        <Link
+                                            href="/topics/questions"
+                                            className={classes.navItemContainer}>
 
-                                            </Link>
-                                        </Col>
-                                    </Row>
-                                    <Row style={{ marginBottom: '10px' }}>
-                                        <Col>
-                                            <Link
-                                                href="/add-topic/questions"
-                                                className={classes.items}
-                                                className={classes.navItemContainer}>
+                                            <i className={`far fa-question-circle ${classes.icon}`}></i>
+                                            <h5 className={classes.navItemText}>Questions</h5>
 
-                                                <i className={`far fa-comments ${classes.icon}`}></i>
-                                                <h5 className={classes.navItemText}>Post a question</h5>
+                                        </Link>
+                                    </Col>
+                                </Row>
+                                <Row style={{ marginBottom: '10px' }}>
+                                    <Col>
+                                        <Link
+                                            href="/add-topic/questions"
 
-                                            </Link>
-                                        </Col>
-                                    </Row>
+                                            className={classes.navItemContainer}>
 
-                                </Col>
+                                            <i className={`far fa-comments ${classes.icon}`}></i>
+                                            <h5 className={classes.navItemText}>Post a question</h5>
 
-                            </Row>
+                                        </Link>
+                                    </Col>
+                                </Row>
 
-                        </Col>
-                    </Row>
-                    <Row style={{ marginBottom: '20px' }}>
-                        <Col>
-                            <Row style={{ marginBottom: '20px' }} >
-                                <Col style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
-                                    <i className="far fa-arrow-alt-circle-right" style={{ fontSize: '17px' }}></i>
-                                    <p className={classes.titles}>Suggestions</p>
-                                </Col>
-                            </Row>
-                            <Row style={{ marginLeft: '10px' }}>
-                                <Col>
-                                    <Row style={{ marginBottom: '10px' }}>
-                                        <Col>
-                                            <Link
-                                                href="/topics/suggestions"
-                                                className={classes.items}
-                                                className={classes.navItemContainer}>
+                            </Col>
 
-                                                <i className={`fas fa-lightbulb ${classes.icon}`}></i>
-                                                <h5 className={classes.navItemText}>Suggestions</h5>
+                        </Row>
 
-                                            </Link>
-                                        </Col>
-                                    </Row>
-                                    <Row style={{ marginBottom: '10px' }}>
-                                        <Col>
-                                            <Link
-                                                href="/add-topic/suggestions"
-                                                className={classes.items}
-                                                className={classes.navItemContainer}>
+                    </Col>
+                </Row>
+                <Row style={{ marginBottom: '20px' }}>
+                    <Col>
+                        <Row style={{ marginBottom: '20px' }} >
+                            <Col style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+                                <i className="far fa-arrow-alt-circle-right" style={{ fontSize: '17px' }}></i>
+                                <p className={classes.titles}>Suggestions</p>
+                            </Col>
+                        </Row>
+                        <Row style={{ marginLeft: '10px' }}>
+                            <Col>
+                                <Row style={{ marginBottom: '10px' }}>
+                                    <Col>
+                                        <Link
+                                            href="/topics/suggestions"
 
-                                                <i className={`far fa-comments ${classes.icon}`}></i>
-                                                <h5 className={classes.navItemText}>Post a Suggestion</h5>
+                                            className={classes.navItemContainer}>
 
-                                            </Link>
-                                        </Col>
-                                    </Row>
-                                </Col>
+                                            <i className={`fas fa-lightbulb ${classes.icon}`}></i>
+                                            <h5 className={classes.navItemText}>Suggestions</h5>
 
-                            </Row>
-                        </Col>
+                                        </Link>
+                                    </Col>
+                                </Row>
+                                <Row style={{ marginBottom: '10px' }}>
+                                    <Col>
+                                        <Link
+                                            href="/add-topic/suggestions"
 
-                    </Row>
-                </Col>
-            </Row>
+                                            className={classes.navItemContainer}>
 
-        </div >
+                                            <i className={`far fa-comments ${classes.icon}`}></i>
+                                            <h5 className={classes.navItemText}>Post a Suggestion</h5>
+
+                                        </Link>
+                                    </Col>
+                                </Row>
+                            </Col>
+
+                        </Row>
+                    </Col>
+
+                </Row>
+            </Col>
+        </Row>
+
     );
 }
 export default Sidebar;
--- components/SidebarRight/SidebarRight.js
+++ components/SidebarRight/SidebarRight.js
@@ -33,132 +33,129 @@ const Sidebar = (props) => {
 
     }, [props.projects])
     return (
-        <div className={classes.sidebar}>
-            <Row>
-                <Col className={classes.content}>
+        <Row className={classes.sidebar}>
+            <Col className={classes.content}>
+                <Row>
                     <Row>
-                        <Row>
 
-                            <Col xs="6">
-                                <p className={classes.titles}>Total Projects</p>
-                            </Col>
+                        <Col xs="6">
+                            <p className={classes.titles}>Total Projects</p>
+                        </Col>
 
-                            <Col style={{ display: 'flex' }}>
+                        <Col style={{ display: 'flex' }}>
 
-                                <p style={{ margin: 'auto' }} className={classes.textItem}>{props.projects.length}</p>
+                            <p style={{ margin: 'auto' }} className={classes.textItem}>{props.projects.length}</p>
 
 
-                            </Col>
-
-                        </Row>
-
+                        </Col>
 
                     </Row>
-                    <hr />
+
+
+                </Row>
+                <hr />
+                <Row>
                     <Row>
-                        <Row>
 
 
-                            <Col>
-                                <p className={classes.titles}>Total Questions</p>
-                            </Col>
+                        <Col>
+                            <p className={classes.titles}>Total Questions</p>
+                        </Col>
 
-                            <Col style={{ display: 'flex' }}>
+                        <Col style={{ display: 'flex' }}>
+                            <Link
+                                href="/topics/questions"
+                                style={{ margin: 'auto' }}
+                                className={classes.items}>
+                                {props.topicsCount.questions}
+                            </Link>
+                        </Col>
+
+                    </Row>
+
+
+                </Row>
+                <hr />
+                <Row>
+                    <Row>
+
+
+                        <Col xs="6">
+                            <p className={classes.titles}>Total Suggestions</p>
+                        </Col>
+
+                        <Col style={{ display: 'flex' }}>
+                            <Link
+                                href="/topics/suggestions"
+                                style={{ margin: 'auto' }}
+                                className={classes.items}>
+                                {props.topicsCount.suggestions}
+
+                            </Link>
+                        </Col>
+
+                    </Row>
+
+
+                </Row>
+                <hr />
+                <Row>
+                    <Row>
+
+
+                        <Col xs="6" xl="6">
+                            <p className={classes.titles}>Most seen project</p>
+                        </Col>
+
+                        <Col xs="6" xl="6">
+
+
+                            {mostSeen ?
                                 <Link
-                                    href="/topics/questions"
-                                    style={{ margin: 'auto' }}
+                                    href={`/project/${mostSeen.name.replace(/ /g, '-')}/${mostSeen.technologie.replace(/ /g, '-')}/${mostSeen._id}`}
                                     className={classes.items}>
-                                    {props.topicsCount.questions}
+                                    {mostSeen.name}
                                 </Link>
-                            </Col>
+                                :
+                                <p className={classes.textItem}>No projects yet!</p>
+                            }
 
-                        </Row>
 
+                        </Col>
 
                     </Row>
-                    <hr />
+
+
+                </Row>
+                <hr />
+                <Row>
                     <Row>
-                        <Row>
 
 
-                            <Col xs="6">
-                                <p className={classes.titles}>Total Suggestions</p>
-                            </Col>
+                        <Col xs="6" xl="6">
+                            <p className={classes.titles}>Most Downloaded project</p>
+                        </Col>
 
-                            <Col style={{ display: 'flex' }}>
-                                <Link
-                                    href="/topics/suggestions"
-                                    style={{ margin: 'auto' }}
-                                    className={classes.items}>
-                                    {props.topicsCount.suggestions}
-
-                                </Link>
-                            </Col>
-
-                        </Row>
-
-
-                    </Row>
-                    <hr />
-                    <Row>
-                        <Row>
-
-
-                            <Col xs="6" xl="6">
-                                <p className={classes.titles}>Most seen project</p>
-                            </Col>
-
-                            <Col xs="6" xl="6">
-
-
-                                {mostSeen ?
+                        <Col xs="6" xl="6">
+                            {
+                                mostDownload ?
                                     <Link
-                                        href={`/project/${mostSeen.name.replace(/ /g, '-')}/${mostSeen.technologie.replace(/ /g, '-')}/${mostSeen._id}`}
+                                        href={`/project/${mostDownload.name.replace(/ /g, '-')}/${mostDownload.technologie.replace(/ /g, '-')}/${mostDownload._id}`}
                                         className={classes.items}>
-                                        {mostSeen.name}
+                                        {mostDownload.name}
                                     </Link>
+
                                     :
                                     <p className={classes.textItem}>No projects yet!</p>
-                                }
-
-
-                            </Col>
-
-                        </Row>
-
+                            }
+                        </Col>
 
                     </Row>
-                    <hr />
-                    <Row>
-                        <Row>
 
 
-                            <Col xs="6" xl="6">
-                                <p className={classes.titles}>Most Downloaded project</p>
-                            </Col>
-
-                            <Col xs="6" xl="6">
-                                {
-                                    mostDownload ?
-                                        <Link
-                                            href={`/project/${mostDownload.name.replace(/ /g, '-')}/${mostDownload.technologie.replace(/ /g, '-')}/${mostDownload._id}`}
-                                            className={classes.items}>
-                                            {mostDownload.name}
-                                        </Link>
-
-                                        :
-                                        <p className={classes.textItem}>No projects yet!</p>
-                                }
-                            </Col>
-
-                        </Row>
-
-
-                    </Row>
-                </Col>
-            </Row>
-
-        </div >
+                </Row>
+            </Col>
+        </Row>
     );
 }
 export default Sidebar;
