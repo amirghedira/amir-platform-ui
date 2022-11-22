55089f33146dc9828fa29446e0c6e6443b8f1fa5
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-22 23:47:48 +0100

    update gitlab-runner

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-REPO_PATH="/home/centos/save-amir-platform-ui"
+
+USER_PATH="/home/almalinux"
+REPO_PATH="${USER_PATH}/save-amir-platform-ui"
 if [ ! -d "${REPO_PATH}" ]; then
-    cd "/home/centos"
+    sudo cd "${USER_PATH}"
     git clone "https://amirghedira:${GITLAB_TOKEN}@gitlab.com/amir-platform/amir-platform-ui.git" save-amir-platform-ui
-    cd save-amir-platform-ui
+    cd save-amir-platform-api
     git remote add github "https://amirghedira:${GITHUB_TOKEN}@github.com/amirghedira/mypersonalweb-nextjs.git"
 fi
 
-cd "${REPO_PATH}" && git pull origin main || :
+sudo cd "${REPO_PATH}" && git pull origin main || :
 git push github main -f
 exit 0
