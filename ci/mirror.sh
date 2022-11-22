4720fbeb7c286245c3744f73191ad0b72401457a
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-22 23:53:29 +0100

    fix mirror.sh

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 
-USER_PATH="/home/almalinux"
-REPO_PATH="${USER_PATH}/save-amir-platform-ui"
+cd /home/almalinux
+pwd
+REPO_PATH="home/almalinux/save-amir-platform-ui"
 if [ ! -d "${REPO_PATH}" ]; then
-    sudo cd "${USER_PATH}"
-    pwd
+
     git clone "https://amirghedira:${GITLAB_TOKEN}@gitlab.com/amir-platform/amir-platform-ui.git" save-amir-platform-ui
     cd save-amir-platform-ui
     git remote add github "https://amirghedira:${GITHUB_TOKEN}@github.com/amirghedira/mypersonalweb-nextjs.git"
 fi
 
-sudo cd "${REPO_PATH}" && git pull origin main || :
+git pull origin main || :
 git push github main -f
 exit 0
