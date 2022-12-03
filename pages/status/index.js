a0ea3c18750fe42992ae022c2f180d270d8345a4
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-12-03 01:57:17 +0100

    add status page

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,10 +1,10 @@
 import Page404 from "../components/Page404/Page404"
 
 
-const Custom404 = () => {
+const StatusPage = () => {
 
     return <Page404 statuscode={200} message={'website is up and running'} />
 }
 
 
-export default Custom404
+export default StatusPage
