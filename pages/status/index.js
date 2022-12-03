31236824b3c2a5d9e33c227d4d70136bb40c1e7f
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-12-03 02:05:37 +0100

    fix status page

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,9 +1,17 @@
-import Page404 from "../components/Page404/Page404"
-
-
 const StatusPage = () => {
 
-    return <Page404 statuscode={200} message={'website is up and running'} />
+    return (
+        <div id="notfound">
+            <div className="notfound">
+                <div className="notfound-404">
+
+                    <h1>200</h1>
+                </div>
+                <h2>Website is up and running.</h2>
+                <a href="/">Back To Homepage</a>
+            </div>
+        </div>
+    )
 }
 
 
